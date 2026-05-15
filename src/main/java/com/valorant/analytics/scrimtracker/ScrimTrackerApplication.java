1070966d602dd7136f9bbf5cad6c69ffdbc63a1f
Roy <145612963+RoyNieto@users.noreply.github.com>
2026-05-14 22:29:32 -0600

    feat: esqueleto inicial de Spring Boot

---
+++ src/main/java/com/valorant/analytics/scrimtracker/ScrimTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.valorant.analytics.scrimtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorant/analytics/scrimtracker/ScrimTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorant.analytics.scrimtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrimTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
